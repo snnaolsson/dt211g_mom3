6bef7024e53becaac6d9d311ab2b5a68f179bd18
Sanna Olsson <saol1901@student.miun.se>
2024-02-18 21:07:52 +0100

    updates to logo and nav. New js for mouseover.

--- src/js/app.js
+++ src/js/app.js
@@ -15,32 +15,3 @@ function burgerMenu() {
     menu.style.display = "none";
   }
 }
-
-let btn = document.getElementById("aurora-header");
-let text = document.getElementById("aurora-text");
-let btnmyt = document.getElementById("myt-header");
-let mytText = document.getElementById("myt-text");
-
-btn.addEventListener("mouseover", showText);
-btn.addEventListener("mouseleave", hideText);
-btnmyt.addEventListener("mouseover", showMyText);
-btnmyt.addEventListener("mouseleave", hideMyText);
-
-function showText() {
-  let compStyle = window.getComputedStyle(text).display;
-  if (compStyle === "none") {
-    text.style.display = "block";
-  }
-}
-function showMyText() {
-  let compStyle = window.getComputedStyle(mytText).display;
-  if (compStyle === "none") {
-    mytText.style.display = "block";
-  }
-}
-function hideText() {
-  text.style.display = "none";
-}
-function hideMyText() {
-  mytText.style.display = "none";
-}
