19cb6ae74cd5ea23b3a436801f9d1f9d88de8b9e
Sanna Olsson <saol1901@student.miun.se>
2024-02-22 09:14:37 +0100

    Function to display the most searched courses.

--- src/js/diagrams.js
+++ src/js/diagrams.js
@@ -1,3 +1,5 @@
+import Chart from "chart.js/auto";
+
 const courseEl = document.getElementById("courses");
 const programEl = document.getElementById("programs");
 
@@ -7,6 +9,7 @@ async function init() {
   displayCourses();
 }
 
+//Funktion för att hämta information från API:t.
 async function loadStatistics() {
   try {
     const response = await fetch("https://studenter.miun.se/~mallar/dt211g/");
@@ -16,7 +19,7 @@ async function loadStatistics() {
     console.log("Något gick fel!");
   }
 }
-
+//Funktion för att sortera kurserna utifrån antal sökande, visa de 6 populäraste kurserna och sedan skriva ut de till gränssnittet i ett stapeldiagram
 async function displayCourses() {
   try {
     const admissionStatistics = await loadStatistics();
@@ -28,6 +31,26 @@ async function displayCourses() {
     });
 
     const coursesTop = courses.slice(0, 6);
+
+    const courseName = coursesTop.map((course) => course.name);
+    const applicants = coursesTop.map((course) =>
+      parseInt(course.applicantsTotal)
+    );
+
+    new Chart(courseEl, {
+      type: "bar",
+      data: {
+        labels: courseName,
+        datasets: [
+          {
+            label: "Antal sökande",
+            data: applicants,
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
+
     console.log(coursesTop);
   } catch (error) {
     console.log("Något gick fel!");
