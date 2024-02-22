50b883358f4027840a889d0705fae90f97bb8ef8
Sanna Olsson <saol1901@student.miun.se>
2024-02-22 09:47:07 +0100

    Function to display the most searched programs

--- src/js/diagrams.js
+++ src/js/diagrams.js
@@ -7,6 +7,7 @@ window.onload = init();
 
 async function init() {
   displayCourses();
+  displayPrograms();
 }
 
 //Funktion för att hämta information från API:t.
@@ -22,7 +23,9 @@ async function loadStatistics() {
 //Funktion för att sortera kurserna utifrån antal sökande, visa de 6 populäraste kurserna och sedan skriva ut de till gränssnittet i ett stapeldiagram
 async function displayCourses() {
   try {
-    const admissionStatistics = await loadStatistics();
+    const admissionStatistics = await loadStatistics(); //Kör funktionen som hämtar datan från API och lagrar i admissionStatistics
+
+    //Filtrerar admissionStatistics utifrån att de ska vara kurser och sparar i variabeln courses. Sorterar sedan dessa flest antal sökande till lägst antal.
     const courses = admissionStatistics.filter((item) => item.type === "Kurs");
     courses.sort((a, b) => {
       const applicantsA = parseInt(a.applicantsTotal);
@@ -30,13 +33,13 @@ async function displayCourses() {
       return applicantsB - applicantsA;
     });
 
-    const coursesTop = courses.slice(0, 6);
+    const coursesTop = courses.slice(0, 6); //Skapar ny variabel och sparar de första 6 kurserna från den sorterade arrayen
 
-    const courseName = coursesTop.map((course) => course.name);
-    const applicants = coursesTop.map((course) =>
-      parseInt(course.applicantsTotal)
+    const courseName = coursesTop.map((course) => course.name); //Skapar en array med kursnamn från objekten i coursesTop
+    const applicants = coursesTop.map(
+      (course) => parseInt(course.applicantsTotal) //// Skapar en array med antalet sökande (som heltal) från objekten i coursesTop
     );
-
+    //Skapar nytt bar-diagram, och använder kursnamnen och antal sökande till kurserna som label och data
     new Chart(courseEl, {
       type: "bar",
       data: {
@@ -50,8 +53,42 @@ async function displayCourses() {
         ],
       },
     });
+  } catch (error) {
+    console.log("Något gick fel!");
+  }
+}
+//Funktion för att sortera programmen utifrån antal sökande, visa de 5 populäraste programmen och sedan skriva ut de till gränssnittet i ett pie-diagram
+async function displayPrograms() {
+  try {
+    const admissionStatistics = await loadStatistics();
+    const programs = admissionStatistics.filter(
+      (item) => item.type === "Program"
+    );
+    programs.sort((a, b) => {
+      const appliA = parseInt(a.applicantsTotal);
+      const appliB = parseInt(b.applicantsTotal);
+      return appliB - appliA;
+    });
 
-    console.log(coursesTop);
+    const programTop = programs.slice(0, 5);
+
+    const programName = programTop.map((programs) => programs.name);
+    const applicants = programTop.map((programs) =>
+      parseInt(programs.applicantsTotal)
+    );
+
+    new Chart(programEl, {
+      type: "pie",
+      data: {
+        labels: programName,
+        datasets: [
+          {
+            label: "Antal sökande",
+            data: applicants,
+          },
+        ],
+      },
+    });
   } catch (error) {
     console.log("Något gick fel!");
   }
